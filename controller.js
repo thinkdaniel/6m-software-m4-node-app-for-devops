8be54069357a7b07d25aac77f0061eaeaa57eb46
Daniel Goh <danielgoh@outlook.com>
2025-08-26 21:19:00 +0800

    Fix lint error

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 
 // This is the only function that will be called by the endpoint.
 function print(req, res){
-    const unused = 42;
+    // const unused = 42;
     res.send("Hello world!!");
 }
 
