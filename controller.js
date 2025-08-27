eb3cd6c6054f113adf8636fac6641350af5fed1f
Daniel Goh <danielgoh@outlook.com>
2025-08-27 16:48:01 +0800

    Test

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 
 // This is the only function that will be called by the endpoint.
 function print(req, res){
-    // For testing lint job
+    // For testing lint job.
     // const unused = 42;
     res.send("Hello world!!");
 }
