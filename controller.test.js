14d8aa3b6b336cc9042972e21f98e84fd275f235
Daniel Goh <danielgoh@outlook.com>
2025-08-28 13:15:36 +0800

    Update to named imports in controller test

--- controller.test.js
+++ controller.test.js
@@ -1,20 +1,19 @@
 /* eslint-disable */
-const targetFn = require("./controller");
+const { print: targetFn } = require("./controller");
 
-describe("the print function", ()=>{
+describe("the print function", () => {
+  // standard best practice
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    // standard best practice
-    beforeEach(()=>{
-        jest.clearAllMocks();
-    })
+  it("should respond with 'Hello world!'", () => {
+    const mockReq = {};
+    const mockRes = {
+      send: jest.fn(),
+    };
 
-    it("should respond with 'Hello world!'", ()=>{
-        const mockReq = {};
-        const mockRes = {
-            send:jest.fn()
-        }
-
-        targetFn(mockReq, mockRes);
-        expect(mockRes.send).toHaveBeenCalledWith("Hello world!!"); // If you change this value, the test will fail.
-    })
-})
+    targetFn(mockReq, mockRes);
+    expect(mockRes.send).toHaveBeenCalledWith("Hello world!!"); // If you change this value, the test will fail.
+  });
+});
