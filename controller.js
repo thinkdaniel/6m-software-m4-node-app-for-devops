03483d703ea0d2526658b99d9ba076d99c559386
Daniel Goh <danielgoh@outlook.com>
2025-08-28 20:47:32 +0800

    Modify text

--- controller.js
+++ controller.js
@@ -2,7 +2,7 @@
 // This is the only function that will be called by the endpoint.
 function print(req, res){
     // const unused = 42;
-    res.send("Hello world!!");
+    res.send("Hello world! It's a great day!");
 }
 
 module.exports = print;
