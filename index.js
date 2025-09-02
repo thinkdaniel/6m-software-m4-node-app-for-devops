53628b7ab1912448ceb4324905e2796293fa0b82
Daniel Goh <danielgoh@outlook.com>
2025-09-02 17:35:36 +0800

    Fix X-Powered-By header vulnerability

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const helmet = require("helmet"); 
 const app = express();
+
+// Use helmet middleware for security
+app.use(helmet());
+
+// Disable X-Powered-By header
+app.disable("x-powered-by");
 
 // This is parse JSON request bodies
 app.use(express.json());
